2f5d2437def358cc9cf9443b8e32e0a9bb3aec3a
Alexandr Perov <PerovAV.home@gmail.com>
2023-05-22 13:25:21 +0300

    Сделан центральный блок и подвал

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,24 @@
+// Добавляем класс active на бургер и меню
 let toggle = document.querySelector('.toggle');
+let menu = document.querySelector('.header__menu');
 toggle.onclick = function() {
 	toggle.classList.toggle('active')
+	menu.classList.toggle('active')
 }
+
+// Добавляем класс active на пункты меню
+// let menu_item = document.querySelectorAll('.header__menu-item');
+// menu_item.forEach(item =>{
+// 	item.onclick = function(e) {
+// 		if(e.currentTarget == item) {
+// 			item.classList.remove('active')
+// 		} else {
+
+// 			item.classList.add('active')
+// 			menu.classList.remove('active')
+// 			toggle.classList.remove('active')
+// 		}
+		
+// 	}
+// })
+
